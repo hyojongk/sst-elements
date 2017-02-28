14b76da275dad0a0c7bd611cf8c190cf37dc0c71
Hughes <chughes@sandia.gov>
2017-02-28 10:14:42 -0700

    Added debug information to WB removal in MSHR.

--- src/sst/elements/memHierarchy/mshr.cc
+++ src/sst/elements/memHierarchy/mshr.cc
@@ -463,6 +463,9 @@ bool MSHR::removeElement(Addr baseAddr, mshrType entry) {
 
 
 void MSHR::removeWriteback(Addr baseAddr) {
+#ifdef __SST_DEBUG_OUTPUT__
+    if (DEBUG_ALL || DEBUG_ADDR == baseAddr) d_->debug(_L9_, "MSHR: Removed writeback.  Key Addr = %" PRIx64 "\n", baseAddr);
+#endif
     MSHR::removeElement(baseAddr, mshrType(baseAddr));
 }
 
