d0733813fdf859b2021530bfccf9e73186d4a580
Arun Rodrigues <afrodri@sandia.gov>
2017-04-21 13:45:44 -0600

    added how to generate events, but didn't do so...

--- src/sst/elements/memHierarchy/membackend/pagedMultiBackend2.cc
+++ src/sst/elements/memHierarchy/membackend/pagedMultiBackend2.cc
@@ -571,7 +571,7 @@ void pagedMultiMemory2::handleMemCompReq(SST::Event *event){
         reqs.erase(reqI);
 
         // request id 0 is used internally  -- is that right?
-#warning we should actually generate proper ids
+#warning we should actually generate proper ids using generateUniqueId()
         if (id != 0) {
             assert(outToSlow.find( id ) != outToSlow.end());
             outToSlow.erase( id );
